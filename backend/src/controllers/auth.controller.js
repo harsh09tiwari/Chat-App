33a01d3e790b90f13a2e15290659e74df0209c42
Harsh <139758128+harsh09tiwari@users.noreply.github.com>
2025-12-11 09:51:41 +0530

    fix: Correct log message in login controller for clarity

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -86,7 +86,7 @@ export const login = async (req, res)=> {
         })
 
     } catch (error) {
-        console.log("Enter in login Controller", error.message);
+        console.log("Error in login Controller", error.message);
         res.status(500).json({message : "Internal Server Error"})
     }
 };
